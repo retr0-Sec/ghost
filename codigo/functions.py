a5b1c2b6e5d3918c8b8dccad79ff83c0d0559081
retr0 <kg3139120@gmail.com>
2026-04-16 11:41:43 -0300

    Melhorias de codigo | adicionando mode ativo e passivo

--- codigo/functions.py
+++ codigo/functions.py
@@ -4,6 +4,7 @@ import numpy as np # mais low level para converter bytes
 import threading
 import datetime
 import subprocess
+import psutil
 import time
 import os
 
@@ -17,8 +18,12 @@ micro = recon.Recognizer()
 
 espera_de_fala = threading.Lock()
 
-# captura de audio
 
+#controle de escuta evitando o bug de ficar ouvindo e executando varias vezes
+
+falando = False
+
+# captura de audio
 def capture_audio():
     # inicia a escuta
 
@@ -39,6 +44,10 @@ def capture_audio():
             return None
 
 def escutar(max_tentativas=3):
+    global falando
+    if falando:
+        return None
+
     for _ in range(max_tentativas):
         comando = normalizar(capture_audio())
 
@@ -50,6 +59,13 @@ def escutar(max_tentativas=3):
     fala("Não consegui ouvir você.")
     return None
 
+def escutar_passivo():
+    # escuta quieto, sem ficar falando para repetir -_-
+    global falando
+    if falando:
+        return None
+    return normalizar(capture_audio())
+
 def normalizar(texto):
     if not texto:
         return None
@@ -57,21 +73,26 @@ def normalizar(texto):
 
 # converte o texto para fala
 def fala(texto):
+    global falando
     with espera_de_fala:
-        som = subprocess.Popen(["aplay", "-f", "S16_LE", "-r", "22050", "-c", "1"], stdin=subprocess.PIPE)
-        for pedaco in voice.synthesize(texto):
-            # converte float → int16 → bytes
-            audio = (pedaco.audio_float_array * 32767).astype(np.int16)
-            som.stdin.write(audio.tobytes())
+        falando = True
+        try:
+            som = subprocess.Popen(["aplay", "-f", "S16_LE", "-r", "22050", "-c", "1"], stdin=subprocess.PIPE)
+            for pedaco in voice.synthesize(texto):
+                # converte float → int16 → bytes
+                audio = (pedaco.audio_float_array * 32767).astype(np.int16)
+                som.stdin.write(audio.tobytes())
 
-        som.stdin.close()
-        som.wait()
+            som.stdin.close()
+            som.wait()
+        finally:
+            falando = False
 
 def Saudacao():
     horario = datetime.datetime.now().hour
     if 6 <= horario < 12:
         print("Bom dia retr0, seja bem vindo!")
-        fala('Bom dia retrô, Como voce ta?, acordou cedo hoje ')
+        fala('Bom dia retrô, Como você ta?, acordou cedo hoje ')
     elif 12 <= horario < 18:
         print("Boa tarde retr0, Como voce está?")
         fala("Boa tarde retrô, como voce tá?, Vamos fazer o que hoje?")
@@ -111,32 +132,19 @@ def abrirYoutube():
         return
 
     fala("Entendi Senhor.")
-    subprocess.Popen(["brave-browser",f"https://youtube.com/results?search_query={query.replace('','+')}"])
+    subprocess.Popen(["brave-browser",f"https://youtube.com/results?search_query={query.replace(' ','+')}"])
 
 def Pesquisa():
-    def escutar(max_tentativas=3):
-        for _ in range(max_tentativas):
-            comando = normalizar(capture_audio())
-
-            if comando:
-                if "não" in comando:
-                    fala("Entendido voltando")
-                    return
-                return comando.lower().strip()
-
-
-            fala("Não entendi, pode repetir?")
-
-        fala("Não consegui ouvir você.")
-        return None
-
     fala("Claro, só me diga, o que voçê quer pesquisar?")
     query = escutar()
     if not query:
         return
-
+    if "não" in query or  "nao" in query:
+        print("retornando..")
+        fala("Ok retrô, Retornando..")
+        return
     fala("Entendido retrõ.")
-    subprocess.Popen(["brave-browser", f"https://www.google.com/search?q={query.replace('', '+')}"])
+    subprocess.Popen(["brave-browser", f"https://www.google.com/search?q={query.replace(' ', '+')}"])
 
 def quem_sou():
     fala("Bom, me chamo Nex, fui criado pelo retrô com código puro, sem Inteligencia Artificial. Fui criado para ter controle total do sistema, e executar ações a partir das ordens me passada. Ainda sou um protótipo, mas estou em desenvolvimento.")
@@ -172,6 +180,13 @@ def estudos():
     time.sleep(2)
     subprocess.Popen("konsole")
 
+def estadoAtual():
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory()
+    print(f"CPU: {cpu}%")
+    print(f"RAM:{ram.percent}")
+    fala(f"Bom retrô, Sua máquina agora está com {cpu} por cento da CPU em uso. Sua memória Ram está em {ram.percent} por cento")
+    fala("pode ficar tranquilo, estou monitorando o sistema e avisarei se algum componente se sobrecarregar.")
 
 def Timer():
     def extrair_segundos(texto):
@@ -181,7 +196,7 @@ def Timer():
             numero = ''.join(filter(str.isdigit, texto))
             return int(numero) if numero else None
 
-        if "minuto" in texto:
+        if "minuto" in texto or "min" in texto:
             numero = ''.join(filter(str.isdigit, texto))
             return (int(numero) * 60) if numero else None
 
@@ -189,7 +204,8 @@ def Timer():
 
     def contar_timer(seg):
         if seg > 60:
-            fala(f"Ok, Iniciando contagem de {seg} minutos conforme você pediu, avisarei assim que terminar..")
+            minutos = seg // 60
+            fala(f"Ok, Iniciando contagem de {minutos} minutos conforme você pediu, avisarei assim que terminar..")
         else:
             fala(f"Ok, Iniciando contagem de {seg} segundos conforme você pediu, avisarei assim que terminar..")
         time.sleep(seg)
@@ -198,8 +214,18 @@ def Timer():
     texto = escutar()
     if not texto:
         fala("Não entendi retrô")
+        return
     seg = extrair_segundos(texto)
     if not seg:
         fala("Não consegui entender a tempo, desculpe")
         return
     threading.Thread(target=contar_timer, args=(seg,)).start()
+
+def monitoramento():
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    if cpu >= 85.0:
+        print(f"CPU:{cpu}%")
+        fala(f"Retrô, sua CPU está Bem alta, Com {cpu} por cento!!. Isso vai travar seu sistema.")
+    if ram > 88:
+        fala(f"retrô Sua Memória RAM está com {ram} por cento.")
--- codigo/main.py
+++ codigo/main.py
@@ -1,8 +1,8 @@
-from GHOST.codigo.functions import *
+from functions import *
 Saudacao()
 comandos = {
     "nex": {
-        "palavras": ["nex","nexi","nexes","néquisis","ness","nexus"],
+        "palavras": ["nex","nexi","nexes","néquisis","ness","nexus","fala"],
         "funcao": Nex
     },
     "horas": {
@@ -22,7 +22,7 @@ comandos = {
         "funcao": Pesquisa
     },
     "voce":{
-        "palavras":["criou","crio"],
+        "palavras":["quem","você é"],
         "funcao":quem_sou
     },
     "desligar":{
@@ -30,7 +30,7 @@ comandos = {
         "funcao": desligar
     },
     "terminal":{
-        "palavras":["terminal","terminau","termi"],
+        "palavras":["terminal"],
         "funcao": terminal
     },
     "processos":{
@@ -42,23 +42,53 @@ comandos = {
         "funcao": estudos
     },
     "time":{
-        "palavras":["taime","time","taimi"],
+        "palavras":["taime","time","taimi","cronômetro"],
         "funcao": Timer
+    },
+    "estado":{
+        "palavras":["estado","maquina","estado"],
+        "funcao": estadoAtual
     }
 }
+timeout = 20 # tempo que ele fica aguardando comandos
+ativamento = ["nex","nexi","néquisis","ness","nexus","fala nex"]
+ativo = False # entra em estado de escuta de comandos
+tempo_ativo = 0 # tempo em que está escutando comando que se renova a cada comando executado
+
 while True:
-    comando = normalizar(capture_audio())
+    monitoramento()
+    comando = escutar_passivo()
 
     if not comando or not comando.strip():
+        if ativo and (time.time() - tempo_ativo > timeout):
+            ativo = False
+            print("[*] Modo de escuta [*]")
+            fala("Voltando ao modo de escuta retrô.")
         continue
-    executado = False
+
     comando = comando.lower()
+    # modo de escuta passiva, sem execução
+    if not ativo:
+        if any(palavra in comando for palavra in ativamento):
+            ativo = True
+            tempo_ativo = time.time()
+            Nex()
+        continue # ignora quando não tiver as palavras do ativamento
+
+    # Modo de ativo
+    if time.time() - tempo_ativo > timeout:
+        ativo = False
+        print("[*] Modo de escuta [*]")
+        continue
+
+    tempo_ativo = time.time()
+
+    # executa os comandos
+    executado = False
     for item in comandos.values():
         if any(palavra in comando for palavra in item["palavras"]):
             item["funcao"]()
             executado = True
-            break
-        if executado:
             break
 
     if not executado:
